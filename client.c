b2150685131f530100aced816ee3aebfc0bb50aa
magne <m.hovdar@gmail.com>
2023-05-14 14:45:54 +0200

    Functional multicast but with memory leak on server? Perhaps

--- client.c
+++ client.c
@@ -15,8 +15,8 @@ That's how we can use DMA and multicast.
 //Defines
 #define SEGMENT_SIZE sizeof(FrameData)
 #define RECEIVER_NODE_ID 8
-#define FRAMEDATA_SEGMENT_ID 4
-#define FRAME_SEGMENT_ID 8
+#define FRAMEDATA_SEGMENT_ID 0
+#define FRAME_SEGMENT_ID 1
 #define ADAPTER_NO 0
 #define NO_FLAGS 0
 #define NO_ARGS 0
@@ -25,10 +25,15 @@ That's how we can use DMA and multicast.
 //Global variables
 sci_error_t err;
 
-sci_desc_t v_dev;
-sci_local_segment_t l_seg;
-sci_map_t l_map;
-void* map_addr;
+sci_desc_t v_dev_frame;
+sci_local_segment_t l_seg_frame;
+sci_map_t l_map_frame;
+volatile void* map_addr_frame;
+
+sci_desc_t v_dev_framedata;
+sci_local_segment_t l_seg_framedata;
+sci_map_t l_map_framedata;
+volatile void* map_addr_framedata;
 
 static void SleepMilliseconds(int  milliseconds)
 {
@@ -42,27 +47,27 @@ if (milliseconds<1000) {
 }
 }
 
-
-int request_segment_size(FrameData* frame_data){
+// Open a set-size framedata segment. This is needed to determine size of the other segment
+int open_framedata_segment(FrameData* frame_data){
     //Open a virtual device
-    SCIOpen(&v_dev, NO_FLAGS, &err);
+    SCIOpen(&v_dev_framedata, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error opening virtual device: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
     //Create a local segment
-    SCICreateSegment(v_dev, &l_seg, FRAMEDATA_SEGMENT_ID, SEGMENT_SIZE, NO_CALLBACK, NO_ARGS, NO_FLAGS, &err);
+    SCICreateSegment(v_dev_framedata, &l_seg_framedata, FRAMEDATA_SEGMENT_ID, SEGMENT_SIZE, NO_CALLBACK, NO_ARGS, SCI_FLAG_BROADCAST, &err);
     if(err != SCI_ERR_OK){
         printf("Error creating local segment: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
     //Prepare the segment for use
-    SCIPrepareSegment(l_seg, ADAPTER_NO, NO_FLAGS, &err);
+    SCIPrepareSegment(l_seg_framedata, ADAPTER_NO, SCI_FLAG_BROADCAST, &err);
     if (err == SCI_ERR_OK){
         //Map the segment into the virtual address space
-        map_addr = SCIMapLocalSegment(l_seg, &l_map, 0, SEGMENT_SIZE, NULL, NO_FLAGS, &err);
+        map_addr_framedata = SCIMapLocalSegment(l_seg_framedata, &l_map_framedata, 0, SEGMENT_SIZE, NULL, NO_FLAGS, &err);
     }
     else{
         printf("Error preparing local segment: %s\n", SCIGetErrorString(err));
@@ -70,57 +75,25 @@ int request_segment_size(FrameData* frame_data){
     }
 
     //Set the segment available
-    SCISetSegmentAvailable(l_seg, ADAPTER_NO, NO_FLAGS, &err);
+    SCISetSegmentAvailable(l_seg_framedata, ADAPTER_NO, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error setting segment available: %s\n", SCIGetErrorString(err));
         return 1;
     }
-
+    
     //Wait for server to write the metadata
 
     printf("Waiting for server to write metadata\n");
-    //Initialize the segment to 0
-    *(int*)map_addr = 0;
-    while(*(int*)map_addr == 0){
+    while(*(int*)map_addr_framedata == 0){
         SleepMilliseconds(100);
     }
     printf("Received data from server\n");
     //Use memcpy to copy the data from the segment to the frame_data struct
-    memcpy(frame_data, (FrameData*)map_addr, sizeof(FrameData));
+    memcpy(frame_data, (FrameData*)map_addr_framedata, sizeof(FrameData));
     printf("Frame data: %d, %d, %d, %d\n", frame_data->width, frame_data->height, frame_data->pix_fmt, frame_data->buffer_size);
     //In later iterations, we can avoid closing the segment for 
     // updates to the frame_data struct if the video format changes
 
-    //For now, we close the segment
-    SCISetSegmentUnavailable(l_seg, ADAPTER_NO, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error setting segment unavailable: %s\n", SCIGetErrorString(err));
-        return 1;
-    }
-    
-    SCISetSegmentUnavailable(l_seg, ADAPTER_NO, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error setting segment unavailable: %s\n", SCIGetErrorString(err));
-        return 1;
-    }
-
-    SCIUnmapSegment(l_map, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error unmapping segment: %s\n", SCIGetErrorString(err));
-        return 1;
-    }
-
-    SCIRemoveSegment(l_seg, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error removing segment: %s\n", SCIGetErrorString(err));
-        return 1;
-    }
-    
-    SCIClose(v_dev, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error closing virtual device: %s\n", SCIGetErrorString(err));
-        return 1;
-    }
     return 0;
 }
 
@@ -135,32 +108,35 @@ int main(int argc, char* argv[]){
     }
 
     FrameData* frame_data = malloc(sizeof(FrameData));
-    request_segment_size(frame_data);
-    printf("Frame data: %d, %d, %d, %d\n", frame_data->width, frame_data->height, frame_data->pix_fmt, frame_data->buffer_size);
+
+    //This is a blocking call. It will wait for the server to write the metadata
+    open_framedata_segment(frame_data);
+
+
 
     //Prepare for frame data transfers
 
     // Open a virtual device
-    SCIOpen(&v_dev, NO_FLAGS, &err);
+    SCIOpen(&v_dev_frame, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error opening virtual device: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
     // Create a local segment
-    SCICreateSegment(v_dev, &l_seg, FRAME_SEGMENT_ID, frame_data->buffer_size , NO_CALLBACK, NO_FLAGS, NO_ARGS, &err);
+    SCICreateSegment(v_dev_frame, &l_seg_frame, FRAME_SEGMENT_ID, frame_data->buffer_size , NO_CALLBACK, NO_ARGS, SCI_FLAG_BROADCAST, &err);
     if(err != SCI_ERR_OK){
         printf("Error creating local segment: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
     
-    SCIPrepareSegment(l_seg, ADAPTER_NO, NO_FLAGS, &err);
+    SCIPrepareSegment(l_seg_frame, ADAPTER_NO, SCI_FLAG_BROADCAST, &err);
     if(err == SCI_ERR_OK){
         size_t offset = 0;
         size_t size = frame_data->buffer_size;
         void* suggested_addr = NULL;
-        map_addr = SCIMapLocalSegment(l_seg, &l_map, offset, size, suggested_addr, NO_FLAGS, &err);
+        map_addr_frame = SCIMapLocalSegment(l_seg_frame, &l_map_frame, offset, size, suggested_addr, NO_FLAGS, &err);
         if (err != SCI_ERR_OK) {
             printf("Error mapping local segment: %s\n", SCIGetErrorString(err));
             return 1;
@@ -173,7 +149,7 @@ int main(int argc, char* argv[]){
 
 
     // Set the segment available
-    SCISetSegmentAvailable(l_seg, ADAPTER_NO, NO_FLAGS, &err);
+    SCISetSegmentAvailable(l_seg_frame, ADAPTER_NO, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error setting segment available: %s\n", SCIGetErrorString(err));
         return 1;
@@ -205,9 +181,9 @@ int main(int argc, char* argv[]){
     // Do I actually or will the texture work as a buffer?
     //uint8_t buffer* = malloc(frame_data->buffer_size)
     //wait for the server to write to the segment
-    *(int*)map_addr = 0;
+    //*(int*)map_addr = 0;
     while(1){
-        if(*(int*)map_addr == 0){
+        if(*(int*)map_addr_frame == 0){
             // Only before the server has written to the segment
             // One frame lasts 33.3 ms so otherwise this would be too much
             printf("Waiting\n");
@@ -217,7 +193,7 @@ int main(int argc, char* argv[]){
             // Hopefully the delay time here works as an actual delay
             printf("We ball\n");
             //Create AVFrame from FrameData
-            int non_sisci_err = update_avframe_from_framedata(frame_data, frame, map_addr);
+            int non_sisci_err = update_avframe_from_framedata(frame_data, frame, map_addr_frame);
             if(non_sisci_err != 0){
                 printf("Error creating AVFrame from FrameData: \n");
                 return 1;
@@ -234,26 +210,53 @@ int main(int argc, char* argv[]){
     av_frame_free(&frame);
     destroy_videoplayer(&vp);
 
-    SCISetSegmentUnavailable(l_seg, ADAPTER_NO, NO_FLAGS, &err);
+    // Clean up framedata segment
+    SCISetSegmentUnavailable(l_seg_framedata, ADAPTER_NO, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error setting segment unavailable: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
-    SCIUnmapSegment(l_map, NO_FLAGS, &err);
+    SCIUnmapSegment(l_map_framedata, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error unmapping segment: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
-    SCIRemoveSegment(l_seg, NO_FLAGS, &err);
+    SCIRemoveSegment(l_seg_framedata, NO_FLAGS, &err);
+    if(err != SCI_ERR_OK){
+        printf("Error removing segment: %s\n", SCIGetErrorString(err));
+        return 1;
+    }
+    
+    SCIClose(v_dev_framedata, NO_FLAGS, &err);
+    if(err != SCI_ERR_OK){
+        printf("Error closing virtual device: %s\n", SCIGetErrorString(err));
+        return 1;
+    }
+
+    // Clean up frame segment
+
+    SCISetSegmentUnavailable(l_seg_frame, ADAPTER_NO, NO_FLAGS, &err);
+    if(err != SCI_ERR_OK){
+        printf("Error setting segment unavailable: %s\n", SCIGetErrorString(err));
+        return 1;
+    }
+
+    SCIUnmapSegment(l_map_frame, NO_FLAGS, &err);
+    if(err != SCI_ERR_OK){
+        printf("Error unmapping segment: %s\n", SCIGetErrorString(err));
+        return 1;
+    }
+
+    SCIRemoveSegment(l_seg_frame, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error removing segment: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
     
-    SCIClose(v_dev, NO_FLAGS, &err);
+    SCIClose(v_dev_frame, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error closing virtual device: %s\n", SCIGetErrorString(err));
         return 1;
--- server.c
+++ server.c
@@ -17,9 +17,9 @@ That's how we can use DMA and multicast.
 
 //Defines
 #define SEGMENT_SIZE sizeof(FrameData)
-#define FRAMEDATA_SEGMENT_ID 4
+#define FRAMEDATA_SEGMENT_ID 0
 #define LOCAL_NODE_ID 4
-#define FRAME_SEGMENT_ID 8
+#define FRAME_SEGMENT_ID 1
 #define RECEIVER_NODE_ID 8
 #define RECEIVER_SEGMENT_ID 4
 #define ADAPTER_NO 0
@@ -30,20 +30,35 @@ That's how we can use DMA and multicast.
 //Global variables
 sci_error_t err;
 
-/*
-sci_desc_t v_dev_local;
-sci_local_segment_t l_seg;
-size_t local_segment_size;
-sci_map_t l_map;
-*/
-sci_desc_t v_dev;
-sci_remote_segment_t r_seg;
-size_t remote_segment_size;
-sci_map_t r_map;
 
-static volatile void* remote_address;
-//static volatile void* local_address;
+sci_desc_t v_dev_frame;
+sci_local_segment_t l_seg_frame;
+sci_remote_segment_t r_seg_frame;
+sci_map_t l_map_frame;
+sci_map_t r_map_frame;
+volatile void* map_addr_frame;
 
+sci_desc_t v_dev_framedata;
+sci_local_segment_t l_seg_framedata;
+sci_remote_segment_t r_seg_framedata;
+sci_map_t l_map_framedata;
+sci_map_t r_map_framedata;
+volatile void* map_addr_framedata;
+
+volatile void* write_address_framedata;
+volatile void* write_address_frame;
+
+static void SleepMilliseconds(int  milliseconds)
+{
+if (milliseconds<1000) {
+    usleep(1000*milliseconds);
+} else if (milliseconds%1000 == 0) {
+    sleep(milliseconds/1000);
+} else {
+    usleep(1000*(milliseconds%1000));
+    sleep(milliseconds/1000);
+}
+}
 
 int main(int argc, char* argv[]){
     
@@ -85,7 +100,7 @@ int main(int argc, char* argv[]){
     //Fill the queue with frames
     struct process_video_args args = {filename, q, queue_size};
     process_video(&args);
-    printf("Done processing video\n");
+    printf("Done pre-processing video\n");
 
     // Pop frames until we get a valid frame
     frame->format = -1;
@@ -103,64 +118,147 @@ int main(int argc, char* argv[]){
         printf("Error updating framedata from AVFrame: \n");
         return 1;
     }
-    printf("Staeting CISCI STUFF\n");
+    printf("Starting SISCI transfer\n");
 
-    // Open a virtual device
-    SCIOpen(&v_dev, NO_FLAGS, &err);
+
+
+
+
+
+    // Open two virtual devices for Framedata and frame transfer
+
+    SCIOpen(&v_dev_framedata, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
         printf("Error opening virtual device: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
-    // Connect to the remote segment
-    SCIConnectSegment(v_dev, &r_seg, RECEIVER_NODE_ID, FRAMEDATA_SEGMENT_ID, ADAPTER_NO, NO_CALLBACK, NO_ARGS, SCI_INFINITE_TIMEOUT, NO_FLAGS ,&err);
+    //Create a local segment
+    SCICreateSegment(v_dev_framedata, &l_seg_framedata, FRAMEDATA_SEGMENT_ID, SEGMENT_SIZE, NO_CALLBACK, NO_ARGS, SCI_FLAG_BROADCAST, &err);
     if(err != SCI_ERR_OK){
-        printf("Error connecting to remote segment: %s\n", SCIGetErrorString(err));
-        return 1;
-    }
-    remote_segment_size = SCIGetRemoteSegmentSize(r_seg);
-    remote_address = (volatile FrameData*) SCIMapRemoteSegment(r_seg, &r_map, 0, remote_segment_size, 0, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error mapping remote segment: %s\n", SCIGetErrorString(err));
+        printf("Error creating local segment: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
+    //Prepare the segment for use
+    SCIPrepareSegment(l_seg_framedata, ADAPTER_NO, SCI_FLAG_BROADCAST, &err);
+    if (err == SCI_ERR_OK){
+        //Map the segment into the virtual address space
+        map_addr_framedata = SCIMapLocalSegment(l_seg_framedata, &l_map_framedata, 0, SEGMENT_SIZE, NULL, NO_FLAGS, &err);
+    }
+    else{
+        printf("Error preparing local segment: %s\n", SCIGetErrorString(err));
+        return 1;
+    }
 
+    //Set the segment available
+    SCISetSegmentAvailable(l_seg_framedata, ADAPTER_NO, NO_FLAGS, &err);
+    if(err != SCI_ERR_OK){
+        printf("Error setting segment available: %s\n", SCIGetErrorString(err));
+        return 1;
+    }
+
+        /* Connect to remote segment */
+    printf("Connect to remote segment .... ");
+
+    do { 
+        SCIConnectSegment(v_dev_framedata,
+                          &r_seg_framedata,
+                          DIS_BROADCAST_NODEID_GROUP_ALL,
+                          FRAMEDATA_SEGMENT_ID,
+                          ADAPTER_NO,
+                          NO_CALLBACK,
+                          NULL,
+                          SCI_INFINITE_TIMEOUT,
+                          SCI_FLAG_BROADCAST,
+                          &err);
+
+        SleepMilliseconds(10);
+
+    } while (err != SCI_ERR_OK);
+
+    printf("Remote segment (id=0x%x) is connected.\n", FRAMEDATA_SEGMENT_ID);
+
+     /* Map remote segment to user space */
+    write_address_framedata = SCIMapRemoteSegment(r_seg_framedata,&r_map_framedata,0, SEGMENT_SIZE ,NULL,SCI_FLAG_BROADCAST,&err);
+    if (err == SCI_ERR_OK) {
+        printf("Remote segment (id=0x%x) is mapped to user space. \n", FRAMEDATA_SEGMENT_ID);         
+    } else {
+        fprintf(stderr,"SCIMapRemoteSegment failed - Error code 0x%x\n",err);
+        return err;
+    } 
+
+    //Transfer framedata info
     // Copy framedata to remote segment
     printf("Copying framedata to remote segment\n");
-    memcpy((FrameData*)remote_address, frame_data, sizeof(FrameData));
+    memcpy((FrameData*)write_address_framedata, frame_data, sizeof(FrameData));
     
     // Flush queue
     non_sisci_err = avframe_Q_flush(q);
 
-    // Unmap and disconnect from remote segment after framedata transfer
-    SCIUnmapSegment(r_map, NO_FLAGS, &err);
+
+    // Create frame SISCI segment
+
+    SCIOpen(&v_dev_frame, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
-        printf("Error unmapping remote segment: %s\n", SCIGetErrorString(err));
+        printf("Error opening virtual device: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
-    SCIDisconnectSegment(r_seg, NO_FLAGS, &err);
+    //Create a local segment
+    SCICreateSegment(v_dev_frame, &l_seg_frame, FRAME_SEGMENT_ID, frame_data->buffer_size, NO_CALLBACK, NO_ARGS, SCI_FLAG_BROADCAST, &err);
     if(err != SCI_ERR_OK){
-        printf("Error disconnecting remote segment: %s\n", SCIGetErrorString(err));
+        printf("Error creating local segment: %s\n", SCIGetErrorString(err));
         return 1;
     }
 
-    // Wait for receiver to finish
-    // There is probably a better way of doing this
-    sleep(2);
-    // Reconnect with new segment size framedata->buffer_size and segment ID DATA_SEGMENT_ID
-    SCIConnectSegment(v_dev, &r_seg, RECEIVER_NODE_ID, FRAME_SEGMENT_ID, ADAPTER_NO, NO_CALLBACK, NO_ARGS, SCI_INFINITE_TIMEOUT, NO_FLAGS ,&err);
-    if(err != SCI_ERR_OK){
-        printf("Error connecting to remote segment: %s\n", SCIGetErrorString(err));
+    //Prepare the segment for use
+    SCIPrepareSegment(l_seg_frame, ADAPTER_NO, SCI_FLAG_BROADCAST, &err);
+    if (err == SCI_ERR_OK){
+        //Map the segment into the virtual address space
+        map_addr_frame = SCIMapLocalSegment(l_seg_frame, &l_map_frame, 0, frame_data->buffer_size, NULL, NO_FLAGS, &err);
+    }
+    else{
+        printf("Error preparing local segment: %s\n", SCIGetErrorString(err));
         return 1;
     }
-    remote_segment_size = SCIGetRemoteSegmentSize(r_seg);
-    remote_address = (volatile void*) SCIMapRemoteSegment(r_seg, &r_map, 0, remote_segment_size, 0, NO_FLAGS, &err);
+
+    //Set the segment available
+    SCISetSegmentAvailable(l_seg_frame, ADAPTER_NO, NO_FLAGS, &err);
     if(err != SCI_ERR_OK){
-        printf("Error mapping remote segment: %s\n", SCIGetErrorString(err));
+        printf("Error setting segment available: %s\n", SCIGetErrorString(err));
         return 1;
     }
+
+    /* Connect to remote segment */
+    printf("Connect to remote segment .... ");
+
+    do { 
+        SCIConnectSegment(v_dev_frame,
+                          &r_seg_frame,
+                          DIS_BROADCAST_NODEID_GROUP_ALL,
+                          FRAME_SEGMENT_ID,
+                          ADAPTER_NO,
+                          NO_CALLBACK,
+                          NULL,
+                          SCI_INFINITE_TIMEOUT,
+                          SCI_FLAG_BROADCAST,
+                          &err);
+
+        SleepMilliseconds(10);
+
+    } while (err != SCI_ERR_OK);
+
+    printf("Remote segment (id=0x%x) is connected.\n", FRAME_SEGMENT_ID);
+
+     /* Map remote segment to user space */
+    write_address_frame = SCIMapRemoteSegment(r_seg_frame,&r_map_frame,0, frame_data->buffer_size ,NULL,SCI_FLAG_BROADCAST,&err);
+    if (err == SCI_ERR_OK) {
+        printf("Remote segment (id=0x%x) is mapped to user space. \n", FRAME_SEGMENT_ID);         
+    } else {
+        fprintf(stderr,"SCIMapRemoteSegment failed - Error code 0x%x\n",err);
+        return err;
+    } 
 
     //start video processing in a separate thread
     pthread_t video_thread;
@@ -189,6 +287,16 @@ int main(int argc, char* argv[]){
         //The first few frames in queue are empty, so we skip them
         //TODO: Fix this
         if(frame->format != -1){
+            // First update frame_data and transfer that
+            non_sisci_err = update_framedata_from_avframe(frame, frame_data);
+            if (non_sisci_err != 0){
+                printf("Error updating framedata from AVFrame: \n");
+                return 1;
+            }
+            memcpy((FrameData*)write_address_framedata, frame_data, sizeof(FrameData));
+
+            //Do something if the size has changed
+
             //Transfer the frame over PCIe:
             printf("Transferring frame\n");
 
@@ -196,7 +304,7 @@ int main(int argc, char* argv[]){
             //PCIe transfer
             int test = 0;
             if(!test){
-            int bytes_written = av_image_copy_to_buffer((uint8_t*) remote_address, remote_segment_size, (const uint8_t **)frame->data, frame->linesize, frame->format, frame->width, frame->height, 1);
+            int bytes_written = av_image_copy_to_buffer((uint8_t*) write_address_frame, frame_data->buffer_size , (const uint8_t **)frame->data, frame->linesize, frame->format, frame->width, frame->height, 1);
                 if (bytes_written < 0) {
                     fprintf(stderr, "Failed to copy AVFrame to buffer\n");
                     continue;
@@ -208,7 +316,7 @@ int main(int argc, char* argv[]){
             if (test){
                 //Create AVFrame from FrameData
                 AVFrame* testframe = av_frame_alloc();
-                int non_sisci_err = update_avframe_from_framedata(frame_data, testframe, remote_address);
+                int non_sisci_err = update_avframe_from_framedata(frame_data, testframe, write_address_frame);
                 if(non_sisci_err != 0){
                     printf("Error creating AVFrame from FrameData: \n");
                     return 1;
@@ -242,23 +350,81 @@ int main(int argc, char* argv[]){
     //Clean up
     destroy_videoplayer(&vp);
     
-    SCIUnmapSegment(r_map, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error unmapping remote segment: %s\n", SCIGetErrorString(err));
-        return 1;
+    // For frame
+    /* Unmap local segment */
+    SCIUnmapSegment(l_map_framedata,NO_FLAGS,&err);
+    
+    if (err == SCI_ERR_OK) {
+        printf("The local segment is unmapped\n"); 
+    } else {
+        fprintf(stderr,"SCIUnmapSegment failed - Error code 0x%x\n",err);
+        return err;
     }
+    
+    /* Remove local segment */
+    SCIRemoveSegment(l_seg_framedata,NO_FLAGS,&err);
+    if (err == SCI_ERR_OK) {
+        printf("The local segment is removed\n"); 
+    } else {
+        fprintf(stderr,"SCIRemoveSegment failed - Error code 0x%x\n",err);
+        return err;
+    } 
+    
+    /* Unmap remote segment */
+    SCIUnmapSegment(r_map_framedata,NO_FLAGS,&err);
+    if (err == SCI_ERR_OK) {
+        printf("The remote segment is unmapped\n"); 
+    } else {
+        fprintf(stderr,"SCIUnmapSegment failed - Error code 0x%x\n",err);
+        return err;
+    }
+    
+    /* Disconnect segment */
+    SCIDisconnectSegment(r_seg_framedata,NO_FLAGS,&err);
+    if (err == SCI_ERR_OK) {
+        printf("The segment is disconnected\n"); 
+    } else {
+        fprintf(stderr,"SCIDisconnectSegment failed - Error code 0x%x\n",err);
+        return err;
+    } 
 
-    SCIDisconnectSegment(r_seg, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error disconnecting remote segment: %s\n", SCIGetErrorString(err));
-        return 1;
+    // FOr frame
+    /* Unmap local segment */
+    SCIUnmapSegment(l_map_frame,NO_FLAGS,&err);
+    
+    if (err == SCI_ERR_OK) {
+        printf("The local segment is unmapped\n"); 
+    } else {
+        fprintf(stderr,"SCIUnmapSegment failed - Error code 0x%x\n",err);
+        return err;
     }
-
-    SCIClose(v_dev, NO_FLAGS, &err);
-    if(err != SCI_ERR_OK){
-        printf("Error closing virtual device: %s\n", SCIGetErrorString(err));
-        return 1;
+    
+    /* Remove local segment */
+    SCIRemoveSegment(l_seg_frame,NO_FLAGS,&err);
+    if (err == SCI_ERR_OK) {
+        printf("The local segment is removed\n"); 
+    } else {
+        fprintf(stderr,"SCIRemoveSegment failed - Error code 0x%x\n",err);
+        return err;
+    } 
+    
+    /* Unmap remote segment */
+    SCIUnmapSegment(r_map_frame,NO_FLAGS,&err);
+    if (err == SCI_ERR_OK) {
+        printf("The remote segment is unmapped\n"); 
+    } else {
+        fprintf(stderr,"SCIUnmapSegment failed - Error code 0x%x\n",err);
+        return err;
     }
+    
+    /* Disconnect segment */
+    SCIDisconnectSegment(r_seg_frame,NO_FLAGS,&err);
+    if (err == SCI_ERR_OK) {
+        printf("The segment is disconnected\n"); 
+    } else {
+        fprintf(stderr,"SCIDisconnectSegment failed - Error code 0x%x\n",err);
+        return err;
+    } 
 
     SCITerminate();
     return 0;
